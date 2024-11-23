300612acce97a112b408050bae61a4f7ea3d06d9
dyiko <ingonyaa@gmail.com>
2024-11-24 02:08:50 +0530

    day_3_array

---
+++ 350. Intersection of Two Arrays II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        if (len(nums1)> len(nums2)):
+            nums1,nums2 = nums2,nums1
+        mapp={}
+        res=[]
+        for num in nums1:
+            mapp[num] = mapp.get(num,0)+1
+        for num in nums2:
+            if mapp.get(num,0)>0:
+                res.append(num)
+                mapp[num]-=1
+        return res    
