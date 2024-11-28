72c38a0c62ce3591a9329cdd7ec9de04ceeb2dba
dyiko <ingonyaa@gmail.com>
2024-11-29 05:05:40 +0530

    Updated 48_Rotate_Image.py

---
+++ 48_Rotate_Image.py
@@ -0,0 +1,9 @@
+from typing import List
+def rotate(self, matrix: List[List[int]]) -> None:
+        n=len(matrix)
+        for i in range(n):
+            for j in range(i+1,n):
+                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+        for rows in matrix:
+            rows.reverse()        
+        return matrix
